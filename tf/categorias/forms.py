2b3bcb748399e7c4452915e328de372acc6d15e9
David Ricardo Torres <torresdavidricardo.red@gmail.com>
2022-12-21 18:27:32 -0300

    Correccion en categorias

--- tf/categorias/forms.py
+++ tf/categorias/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from django.urls import reverse_lazy
-from bootstrap_modal_forms.forms import BSModalModelForm, BSModalForm
+
 from .models import Categorias
 
 class CategoriasForm(forms.ModelForm):
@@ -19,12 +19,3 @@ class CategoriasFormEditar(forms.ModelForm):
         fields=["nombre"]
 
 
-
-class BookModelForm(BSModalModelForm):
-    publication_date = forms.DateField(
-        error_messages={'invalid': 'Enter a valid date in YYYY-MM-DD format.'}
-    )
-
-    class Meta:
-        model = Categorias
-        exclude = ['timestamp']
